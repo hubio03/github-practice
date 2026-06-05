1d14bf254f229938e028035ba9c0635ec4b3d23e
hubio03 <blackbear1903@gmail.com>
2026-06-05 14:52:13 +0900

    Add multiply function && fix the error

--- calculator.py
+++ calculator.py
@@ -4,11 +4,16 @@ def add(a, b):
 	return a + b
 	
 	
-def subtract(a, b)
+def subtract(a, b):
 	"""두 수를 뺀다."""
 	return a - b
 	
 	
+def multiply(a, b):
+    """두 수를 곱한다."""
+    return a * b
+    
+    
 if __name__ == "__main__":
 	print("2 + 3 = ", add(2, 3))
 	print("5 - 1 = ", substract(5, 1))
